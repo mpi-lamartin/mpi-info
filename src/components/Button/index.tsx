d01582bf43c4be3a70db648131ba5ea9b165f7a1
Quentin Fortier <qpfortier@gmail.com>
2024-09-11 10:40:08 +0000

    Update

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,7 +8,7 @@ export default ({ pdf, cor }): JSX.Element => {
     <div>
       <p>
         <center>
-          <Button sx={{ mx: "auto", m: -2 }} variant="contained" onClick={() => { setBool((b) => !b) }}>{bool ? "Corrigé" : "Énoncé"}</Button>
+          <Button sx={{ mx: "auto", m: -2 }} variant="contained" onClick={() => { setBool((b) => !b) }}>{bool ? "Énoncé" : "Corrigé"}</Button>
         </center>
       </p>
       <Pdf pdf={bool ? cor : pdf} td={true} />
