cbdf710eda948db410e3058f0596f1f1cc521e4e
Quentin Fortier <qpfortier@gmail.com>
2024-11-09 14:50:38 +0100

    Update

--- src/components/Solution/index.tsx
+++ src/components/Solution/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import CodeBlock from '@theme/CodeBlock';
 import clsx from 'clsx';
-import { Details as DetailsGeneric } from '@docusaurus/theme-common/Details';
+import {Details} from '@docusaurus/theme-common/Details';
 import MDXContent from '@theme/MDXContent';
 
 import styles from './styles.module.css';
@@ -11,11 +11,11 @@ const InfimaClasses = 'alert alert--success';
 export default ({ file, lang, show, children }): JSX.Element => {
   return (
     <div> {show &&
-      <DetailsGeneric
+      <Details
         className={clsx(InfimaClasses, styles.details)} summary="Solution">
         {children && <MDXContent>{children}</MDXContent>}
         <CodeBlock language={lang}>{file}</CodeBlock>
-      </DetailsGeneric>
+      </Details>
     }
     </div>
   );
