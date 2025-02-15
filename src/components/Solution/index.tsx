4674496f063cb0125cd0c0d51d8afbe3e4df6f96
Quentin Fortier <qpfortier@gmail.com>
2025-02-15 18:31:02 +0100

    Update

--- src/components/Solution/index.tsx
+++ src/components/Solution/index.tsx
@@ -8,11 +8,11 @@ import styles from './styles.module.css';
 
 const InfimaClasses = 'alert alert--success';
 
-export default ({ file, lang, show, children }): JSX.Element => {
+export default ({ file, lang, show, title="Solution", children }): JSX.Element => {
   return (
     <div> {show &&
       <Details
-        className={clsx(InfimaClasses, styles.details)} summary="Solution">
+        className={clsx(InfimaClasses, styles.details)} summary={title}>
         {children && <MDXContent>{children}</MDXContent>}
         <CodeBlock language={lang}>{file}</CodeBlock>
       </Details>
