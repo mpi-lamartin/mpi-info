bbdef5d479e099c60d06ff3a0e5910ffcf616392
Quentin Fortier <qpfortier@gmail.com>
2025-02-16 22:10:38 +0100

    Update

---
+++ docs/tp/11_to11/sujet_processus/boite_noire1.h
@@ -0,0 +1,7 @@
+#define OBJECTIF 100
+
+void boite_noire(); // à appeler OBJECTIF fois
+
+void demarre_boite(); // à appeler avant tout appel à boite_noire
+
+void eteint_boite(); // à appeler à la fin du programme
