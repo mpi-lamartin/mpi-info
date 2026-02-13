b45ea395e869d2bd9c2d0512763d78d52b116a14
Quentin Fortier <qpfortier@gmail.com>
2026-02-13 22:28:52 +0000

    fix: restore shuffle function implementation for randomizing question order in QCMRandom component

--- src/components/QCMRandom/index.tsx
+++ src/components/QCMRandom/index.tsx
@@ -67,10 +67,10 @@ function MathText({ text }: { text: string }): JSX.Element {
 
 function shuffle<T>(arr: T[]): T[] {
   const a = [...arr];
-  // for (let i = a.length - 1; i > 0; i--) {
-  //   const j = Math.floor(Math.random() * (i + 1));
-  //   [a[i], a[j]] = [a[j], a[i]];
-  // }
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
   return a;
 }
 
